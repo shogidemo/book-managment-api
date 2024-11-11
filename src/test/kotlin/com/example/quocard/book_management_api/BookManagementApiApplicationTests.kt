fa71bae5ca771a2378df68c37b19e43950433399
SATO Sho <shogidemo@gmail.com>
2024-11-11 23:25:19 +0900

    initial commit

---
+++ src/test/kotlin/com/example/quocard/book_management_api/BookManagementApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.quocard.book_management_api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BookManagementApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
